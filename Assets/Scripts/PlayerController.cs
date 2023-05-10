46b2faba0cd14f65b1137fcb3bb649d29cc173d1
Student <student@V314-ENT-007.local>
2023-05-10 16:09:52 -0400

    Project Added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    public float speed = 5; 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        //float yMove = Input.GetAxis("Vertical");
+
+        transform.Translate(x: xMove * speed * Time.deltaTime,  0, 0);
+
+    }
+    
+        
+    
+
+
+
+}
+
+
+
+
+
+
+
+
+  
+
